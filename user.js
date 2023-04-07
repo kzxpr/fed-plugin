b8a69c8a645caac5bdb97d7a71a347bf2926f916
kzxpr <kzxpr@protonmail.com>
2023-04-07 20:40:24 +0200

    Moving inbox stuff into separate handleFiles

--- user.js
+++ user.js
@@ -19,6 +19,14 @@ const { addMessage, removeMessage, updateMessage } = require('./lib/addMessage')
 const { addActivity } = require("./lib/addActivity")
 const { verifySign, makeDigest } = require("./lib/verifySign");
 const { Message, Account } = require('./models/db');
+const { handleCreate } = require('./lib/handleCreate');
+const { handleFollow } = require('./lib/handleFollow');
+const { handleLike } = require('./lib/handleLike');
+const { handleAnnounce } = require('./lib/handleAnnounce');
+const { handleUndo } = require('./lib/handleUndo');
+const { handleDelete } = require('./lib/handleDelete');
+const { handleUpdate } = require('./lib/handleUpdate');
+const { handleAccept } = require('./lib/handleAccept');
 
 router.get('/:username', async function (req, res) {
     const aplog = await startAPLog(req)
@@ -188,14 +196,16 @@ router.get("/:username/inbox", async(req, res) => {
     res.sendStatus(404)
 })
 
+async function unhandled(obj){
+    return new Promise((resolve, reject) => {
+        reject(obj)
+    })
+}
+
 router.post(['/inbox', '/:username/inbox'], async function (req, res) {
     const username = req.params.username || "!shared!";
     let domain = req.app.get('domain');
     const aplog = await startAPLog(req)
-    
-    //console.log(req.body.actor)
-    const myURL = new URL(req.body.actor);
-    let targetDomain = myURL.hostname;
     const reqtype = req.body.type;
 
     console.log("POST", clc.blue("/inbox"), "to "+username+" ("+reqtype+") from "+req.body.actor)
@@ -232,7 +242,8 @@ router.post(['/inbox', '/:username/inbox'], async function (req, res) {
     try{
         const proceed = await addActivity(req.body)
         if(!proceed){
-            throw new Error("Activity already in DB")
+            //throw new Error("Activity already in DB")
+            await unhandled({ statuscode: 200, msg: "Activity already in DB" })
         }
     } catch(e) {
         // IGNORE!!!!!
@@ -241,291 +252,39 @@ router.post(['/inbox', '/:username/inbox'], async function (req, res) {
         res.sendStatus(200)
     }
 
-    
-    
-            const sender = await Account.query().where("uri", "=", req.body.actor)
-                .then((rows) => {
-                    if(rows.length==1){
-                        return rows[0]
-                    }else{
-                        console.warn("Ingen ACTOR in apaccounts...", req.body.actor)
-                        return {};
-                    }
-                })
-
-            if(reqtype === 'Create'){
-                const objtype = req.body.object.type;
-                if(objtype==="Note"){
-                    await addMessage(req.body.object)
-                    .then((d) => {
-                        console.log("I created a note saying",req.body.object)
-                    })
-                    .catch((e) => {
-                        console.error("ERROR in /inbox", e)
-                    })
-                    await endAPLog(aplog, "Received note", 201)
-                    res.sendStatus(201)
-                }else if(objtype==="Article"){
-                    console.log("I got a article saying",req.body.object.content)
-                    addMessage(req.body.object)
-                    await endAPLog(aplog, "Received article", 201)
-                    res.sendStatus(201)
-                }else if(objtype=="Question"){
-                    console.log("I got a question saying", req.body.object.content)
-                    addMessage(req.body.object)
-                    await endAPLog(aplog, "Received question", 201)
-                    res.sendStatus(201)
-                }else{
-                    await endAPLog(aplog, "Received create, but object type wasn't recognized", 500)
-                    console.warn("UNHANDLED RECEIVED", objtype)
-                    res.sendStatus(500)
-                }
-            }else if(reqtype == 'Follow'){
-                if(typeof req.body.object === 'string'){
-                    const local_uri = req.body.object;
-                    await Account.query().where("uri", "=", local_uri).first()
-                    .then(async(account) => {
-                        if(account){
-                            const follower_uri = req.body.actor;
-                            await lookupAccountByURI(follower_uri)
-                                .then(async(follower_account) => {
-
-                                    await addFollower(local_uri, follower_uri)
-                                    await sendAcceptMessage(req.body, local_uri, targetDomain, domain)
-                                    await sendLatestMessages(follower_uri, local_uri)
-                                    .then(async(d) => {
-                                        await endAPLog(aplog, "Pinned messages were sent to new follower: "+follower_uri)
-                                        res.sendStatus(200)
-                                    })
-                                    .catch(async(e) => {
-                                        console.error("ERROR in sendLatestMessages", e)
-                                        await endAPLog(aplog, "ERROR in sendLatestMessages", 500)
-                                        res.sendStatus(500)
-                                    })
-                                })
-                                .catch((err) => {
-                                    console.error(err)
-                                    console.error("ERROR doing lookupAccountByURI", follower_uri)
-                                    res.sendStatus(500)
-                                })
-                        }else{
-                            res.sendStatus(404)
-                        }
-                    });
-                }else{
-                    console.error("I got a follow request I can't handle because object is not a string!", req.body.object)
-                    res.sendStatus(500)
-                }
-            }else if(reqtype == 'Like'){
-                if(typeof req.body.object === 'string'){
-                    const message_uri = req.body.object;
-                    await Message.query().where("uri", "=", message_uri).first()
-                    .then(async(message) => {
-                        console.log("Message is here!")
-                        if(message){
-                            const follower_uri = req.body.actor;
-                            await lookupAccountByURI(follower_uri)
-                                .then(async(follower_account) => {
-                                    await addLike(message_uri, follower_uri)
-                                    await endAPLog(aplog, "Added like")
-                                    res.sendStatus(200)
-                                });
-                        }else{
-                            await endAPLog(aplog, "ERROR: Message to like not found", 404)
-                            res.sendStatus(404)
-                        }
-                    })
-                    .catch(async(e) => {
-                        await endAPLog(aplog, "ERROR looking up message to like", 500)
-                        res.sendStatus(500)
-                    })
-                }else{
-                    await endAPLog(aplog, "Type of like object is not string", 400)
-                    res.sendStatus(400)
-                }
-            }else if(reqtype == 'Announce'){
-                if(typeof req.body.object === 'string'){
-                    const message_uri = req.body.object;
-                    await Message.query().where("uri", "=", message_uri).first()
-                    .then(async(message) => {
-                        if(message){
-                            const follower_uri = req.body.actor;
-                            await lookupAccountByURI(follower_uri)
-                                .then(async(follower_account) => {
-                                    await addAnnounce(message_uri, follower_uri)
-                                    await endAPLog(aplog, "Added announce")
-                                    res.sendStatus(200)
-                                })
-                                .catch(async(e) => {
-                                    await endAPLog(aplog, "ERROR in addAnnounce: "+e, 500)
-                                    res.sendStatus(500)
-                                })
-                        }else{
-                            await endAPLog(aplog, "Message not found", 404)
-                            res.sendStatus(404)
-                        }
-                    })
-                    .catch(async(e) => {
-                        await endAPLog(aplog, "ERROR looking up message to like", 500)
-                        res.sendStatus(500)
-                    })
-                }else{
-                    console.error("Type of like object is not string")
-                    res.sendStatus(400)
-                }
-            }else if(reqtype == 'Undo'){
-                if(typeof req.body.object === 'object'){
-                    const undo_object = req.body.object;
-                    const undo_type = undo_object.type;
-                    const actor = undo_object.actor;
-                    const object = undo_object.object;
-
-                    if(undo_type=="Follow"){    
-                        await removeFollower(object, actor)
-                            .then(async(d) => {
-                                await endAPLog(aplog, "Removed follower")
-                                res.sendStatus(200)
-                            })
-                            .catch(async(e) => {
-                                await endAPLog(aplog, "Some error while removing follower: "+e, 500)
-                                res.sendStatus(500)
-                            })
-                    }else if(undo_type=="Like"){
-                        await removeLike(object, actor)
-                            .then(async(d) => {
-                                await endAPLog(aplog, "Removed like")
-                                res.sendStatus(200)
-                            })
-                            .catch(async(e) => {
-                                await endAPLog(aplog, "Some error while removing like: "+e, 500)
-                                res.sendStatus(500)
-                            })
-                    }else if(undo_type=="Announce"){
-                        await removeAnnounce(object, actor)
-                            .then(async(d) => {
-                                await endAPLog(aplog, "Removed announce")
-                                res.sendStatus(200)
-                            })
-                            .catch(async(e) => {
-                                await endAPLog(aplog, "Some error while removing announce: "+e, 500)
-                                res.sendStatus(500)
-                            })
-                    }else{
-                        await endAPLog(aplog, "Unknown undo-type"+undo_type, 500)
-                        res.sendStatus(500)
-                    }
-                }else{
-                    await endAPLog(aplog, "Can't handle non-object for Undo", 500)
-                    res.sendStatus(500)
-                }
-            }else if(reqtype=="Delete"){
-                const actor = req.body.actor;
-                const object = req.body.object;
-
-                // HOW TO DETERMINE TYPE OF DELETE...
-                if(actor == object){
-                    // PROBABLY WANT TO DELETE USER...
-                    // TO-DO: This should cascade!
-                    await removeAccount(actor)
-                        .then(async(msg) => {
-                            await endAPLog(aplog, msg)
-                            res.sendStatus(200)
-                        })
-                        .catch(async(e) => {
-                            console.log("PROBLEM in removeAccount", e)
-                            await endAPLog(aplog, e, 500)
-                            res.sendStatus(500)
-                        })
-                }else if(typeof object === 'object'){
-                    const msg_id = object.id;
-                    await removeMessage(msg_id, actor)
-                        .then(async(msg) => {
-                            await endAPLog(aplog, msg)
-                            res.sendStatus(200)
-                        })
-                        .catch(async(e) => {
-                            await endAPLog(aplog, e, 500)
-                            res.sendStatus(500)
-                        })
-                }else{
-                    await endAPLog(aplog, "No idea what to do???", 500)
-                    res.sendStatus(500)
-                }
-            }else if(reqtype=="Update"){
-                // RECEIVE UPDATE
-                console.log("UPDATE TRIGGER")
-
-                // GET VALUES
-                const actor = req.body.actor;
-                const object = req.body.object;
-                const id = object.id;
-
-                if(typeof object === "object"){
-                    if(object.type=="Person"){
-                        // UPDATE
-                        if(id==actor){
-                            // ALLOWED
-                            await updateAccount(object)
-                            .then(async(msg) => {
-                                await endAPLog(aplog, msg)
-                                res.sendStatus(200)
-                            })
-                            .catch(async(e) => {
-                                await endAPLog(aplog, "ERROR from updateAccount: "+e)
-                                res.sendStatus(500)
-                            })
-                        }else{
-                            await endAPLog(aplog, "Update denied: Actor "+actor+" cannot change Account for "+id, 401)
-                            res.sendStatus(401)
-                        }
-                    }else{
-                        // ASSUMING THIS GOES TO MESSAGES...
-                        if(object.attributedTo == actor){
-                            // ALLOWED
-                            await updateMessage(object)
-                                .then(async(msg) => {
-                                    await endAPLog(aplog, msg)
-                                    res.sendStatus(200)
-                                })
-                                .catch(async(e) => {
-                                    await endAPLog(aplog, "ERROR from updateMessage: "+e)
-                                    res.sendStatus(500)
-                                })
-                            // TO-DO: This should also influence 'to', 'cc', 'hashtags' and other related tables
-                        }else{
-                            await endAPLog(aplog, "Update denied: Actor "+actor+" cannot change Message for "+id, 401)
-                            res.sendStatus(401)
-                        }
-                    }
-                }else{
-                    await endAPLog(aplog, "Don't know how to handle non-object Update", 500)
-                    res.sendStatus(500)
-                }
-            }else if(reqtype=="Accept"){
-                const accept_from = req.body.actor;
-                const accept_to = req.body.object.actor;
-                const accept_id = req.body.object.id;
-
-                // TO-DO: Confirm accept_id to make sure
-                // that the follow request has actually been sent!
-                
-                // TO-DO: Add a 'update' to old followers
-                // to update the list of followers
-
-                await addFollower(accept_from, accept_to).then(async(msg) => {
-                    await endAPLog(aplog, "A follow was accepted ("+accept_from+" to "+accept_to+"): "+msg)
-                    res.sendStatus(200)
-                })
-                .catch(async(e) => {
-                    await endAPLog(aplog, "ERROR while adding follower "+accept_from+" to "+accept_to+": "+e, 500)
-                    res.sendStatus(500)
-                })
-                
-            }else{
-                await endAPLog(aplog, "REQ type is not recognized...", 400)
-                res.sendStatus(400)
-            }
-    
+    try {
+        let resp;
+        console.log("Let's handle reqtype", reqtype)
+        switch(reqtype){
+            case 'Create': resp = await handleCreate(req.body); break;
+            case 'Follow': resp = await handleFollow(req.body, domain); break;
+            case 'Like': resp = await handleLike(req.body); break;
+            case 'Announce': resp = await handleAnnounce(req.body); break;
+            case 'Undo': resp = await handleUndo(req.body); break;
+            case 'Delete': resp = await handleDelete(req.body); break;
+            case 'Update': resp = await handleUpdate(req.body); break;
+            case 'Accept': resp = await handleAccept(req.body); break;
+            default:
+                resp = await unhandled({ statuscode: 500, msg: "REQ type not recognized in /inbox"});
+                break;
+        }
+        const statuscode = resp.statuscode || 200;
+        const data = resp.data || null;
+        const msg = resp.msg || "";
+        console.log(clc.green("SUCCESS"), msg)
+        if(statuscode != 200){
+            await endAPLog(aplog, msg, statuscode)
+            res.sendStatus(statuscode);
+        }else{
+            await endAPLog(aplog, msg)
+            res.send(data)
+        }
+        
+    } catch(e) {
+        console.log(e.msg)
+        const statuscode = e.statuscode || 500;
+        res.status(statuscode);
+    }    
 });
 
 router.get("*", async(req, res) => {
