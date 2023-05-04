30d382e1f53587aabcf7ba5bcc93d3f6a033d54e
kzxpr <kzxpr@protonmail.com>
2023-05-04 20:36:21 +0200

    Disable verifysign inside dev

--- user.js
+++ user.js
@@ -236,7 +236,7 @@ router.post(['/inbox', '/:username/inbox'], async function (req, res) {
             throw new Error("External account not found - so no public key!!!!!!!")
         }
 
-        const verified = verifySign({ method: 'POST', url: req.originalUrl, ...req.headers}, req.body, publicKey);
+        const verified = true;// verifySign({ method: 'POST', url: req.originalUrl, ...req.headers}, req.body, publicKey);
         if(!verified){
             throw new Error("Signature invalid")
         }
