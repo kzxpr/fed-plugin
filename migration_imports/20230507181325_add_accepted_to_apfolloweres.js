43cd9485fd2a5b70ff641ee0a255028a06790d4b
kzxpr <kzxpr@protonmail.com>
2023-05-07 21:23:05 +0200

    Forgot migration!

---
+++ migration_imports/20230507181325_add_accepted_to_apfolloweres.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('apfollowers', function(t) {
+        t.boolean('accepted').defaultTo(0)
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('apfollowers', function(t) {
+        t.dropColumn('accepted');
+    });
+};
